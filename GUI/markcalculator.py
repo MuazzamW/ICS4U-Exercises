b1c53d0d2117a966a29740a4ee4f46a2672489eb
Muazzam Waheed <waheedmuazzam@gmail.com>
2024-04-13 13:54:48 -0400

    created local

--- GUI/markcalculator.py
+++ GUI/markcalculator.py
@@ -1,13 +1,9 @@
 import tkinter as tk
 
-import tkinter as tk
-from tkinter import tk
-
-
 
 class Window(tk.Tk):
-
-  def __init__(self):
+    
+    def __init__(self):
       super().__init__()
 
       self.title("GUI Mark Calculator")
@@ -16,16 +12,19 @@ class Window(tk.Tk):
       self.submit.grid(row = 0, column = 2, padx = 20, pady = 20)
 
 class submit_button(Window):
-
-  def __init__(self):
+    def __init__(self):
       super().__init__()
       self.submit_pop_up = tk.Toplevel(self)
       self.submit_pop_up.withdraw()
       print(self.submit_pop_up)
-
-  def click_submit_button(self):
+      
+    def click_submit_button(self):
       self.submit_pop_up.deiconify()
       print('New PopUp')
+
+
+
+
 
 if __name__ == "__main__":
     app = submit_button()
